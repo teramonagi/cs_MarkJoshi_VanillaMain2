d92c74d84dca18050914ffc878cb0ecf5d2b3820
unknown <yana@.(none)>
2011-10-08 17:32:13 +0900

    First Commit

---
+++ VanillaMain2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VanillaMain2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //parameters
+            double expiry = 1.0;
+            double strike = 50.0;
+            double spot = 49.0;
+            double vol = 0.2;
+            double r = 0.01;
+
+            //input number of montecarlo paths
+            ulong number_of_paths = 10000;
+            Console.Write("Enter number of montecarlo paths : ");
+            number_of_paths = ulong.Parse(Console.ReadLine());
+
+            //create payoff & option object
+            PayOff payoff1 = new PayOffCall(strike);
+            VanillaOption option1 = new VanillaOption(payoff1, expiry);
+            VanillaOption option2 = new VanillaOption(payoff1, expiry);
+            
+            //montecarlo simulation & output result
+            double result = SimpleMC4.SimpleMonteCarlo3(option1, spot, vol, r, number_of_paths);
+            Console.WriteLine("the call price is " + result.ToString());
+            result = SimpleMC4.SimpleMonteCarlo3(option2, spot, vol, r, number_of_paths);
+            Console.WriteLine("the call price is " + result.ToString());
+            
+            //other payoff
+            PayOff payoff2 = new PayOffPut(strike);
+            VanillaOption option3 = new VanillaOption(payoff2, expiry);
+            option1 = option3.DeepCopy();
+            result = SimpleMC4.SimpleMonteCarlo3(option1, spot, vol, r, number_of_paths);
+            Console.WriteLine("the put price is " + result.ToString());
+            
+        
+        }
+    }
+}
---
+++ VanillaMain2/Vanilla2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VanillaMain2
+{
+    class VanillaOption
+    {
+        public VanillaOption(PayOff payoff_, double expiry_)
+        {
+            this.payoff = payoff_.Clone();
+            this.Expiry = expiry_;
+        }
+        public VanillaOption(VanillaOption original)
+        {
+            this.payoff = original.payoff.Clone();
+            this.Expiry = original.Expiry;
+        }
+        public VanillaOption DeepCopy()
+        {
+            return new VanillaOption(this);
+        }
+        public double PayOff(double spot_)
+        {
+            return payoff.Do(spot_);
+        }
+
+        public double Expiry{get; private set;}
+        private PayOff payoff;
+    }
+}
